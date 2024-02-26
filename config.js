aec46e0b526f68b48011a66e6643833338f2b68f
Harsh Kumar Sinha <hksinha960@gmail.com>
2024-02-27 02:00:37 +0530

    Trying something

--- config.js
+++ config.js
@@ -1,8 +1,8 @@
 import mysql from "mysql";
 
 export var connection = mysql.createConnection({
-  host: process.env.DB_HOST || "localhost",
-  user: process.env.DB_USER || "root",
-  password: process.env.DB_PASSWORD || "123456789",
-  database: process.env.DB_DATABASE || "amongus",
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_DATABASE,
 });
